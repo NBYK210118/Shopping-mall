6099bc08b25f1de8d90fd2c0db003e8ee33e24cb
Lee <eunho9932@gmail.com>
2024-03-03 00:39:05 +0900

    2403030038 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext, useEffect, useState } from 'react';
 import { Images } from './images_list';
 import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
@@ -9,19 +8,6 @@ import { Products } from './components/products_list';
 import SignUp from './components/user/signUp';
 import AuthContext, { AuthProvider } from './auth.context';
 import Loading from './loading';
-import DataService from './data_services';
-=======
-import React, { useContext, useEffect, useState } from "react";
-import { Images } from "./images_list";
-import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
-import SignIn from "./components/signIn";
-import MainContent from "./components/main_content";
-import Mypage from "./components/user/Mypage";
-import { Products } from "./components/products_list";
-import SignUp from "./components/user/signUp";
-import AuthContext, { AuthProvider } from "./auth.context";
-import Loading from "./loading";
->>>>>>> 346d54e0735b8eefad92176e1fac3b06c2dedad6
 
 function MainHeader() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -31,7 +17,7 @@ function MainHeader() {
   const [isScrolled, setIsScrolled] = useState(false);
 
   const goHome = () => {
-    navigate("/");
+    navigate('/');
   };
 
   const toggleMenu = (e) => {
@@ -56,30 +42,30 @@ function MainHeader() {
 
   const handleLogout = () => {
     try {
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("user");
-      localStorage.removeItem("products");
-      localStorage.removeItem("product");
-      localStorage.removeItem("activeMenu");
-      navigate("/home");
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('user');
+      localStorage.removeItem('products');
+      localStorage.removeItem('product');
+      localStorage.removeItem('activeMenu');
+      navigate('/home');
       window.location.reload();
     } catch (error) {
-      navigate("/home");
+      navigate('/home');
     }
   };
 
   useEffect(() => {
-    document.addEventListener("click", closeMenu);
+    document.addEventListener('click', closeMenu);
 
     return () => {
-      document.removeEventListener("click", closeMenu);
+      document.removeEventListener('click', closeMenu);
     };
   }, []);
 
   const menus = () => {
     const result = [
-      { to: "/home", text: "Home" },
-      { to: "/support", text: "Support" },
+      { to: '/home', text: 'Home' },
+      { to: '/support', text: 'Support' },
     ];
 
     const links = result.map((val, idx) => (
@@ -94,7 +80,6 @@ function MainHeader() {
   };
 
   useEffect(() => {
-<<<<<<< HEAD
     const handleScroll = () => {
       if (window.scrollY > 70) {
         setIsScrolled(true);
@@ -112,9 +97,7 @@ function MainHeader() {
 
   useEffect(() => {
     console.log('OpenSearchBox: ', openSearchBox);
-=======
-    console.log("OpenSearchBox: ", openSearchBox);
->>>>>>> 346d54e0735b8eefad92176e1fac3b06c2dedad6
+    console.log('OpenSearchBox: ', openSearchBox);
   }, [openSearchBox]);
 
   return (
@@ -125,10 +108,7 @@ function MainHeader() {
       }`}
     >
       {/*헤더 메뉴 버튼*/}
-      <div
-        id="main_header_menus"
-        className="mw-md:ml-5 mw-md:relative miw-md:w-[150px] h-full flex justify-between"
-      >
+      <div id="main_header_menus" className="mw-md:ml-5 mw-md:relative miw-md:w-[150px] h-full flex justify-between">
         <div className="w-[10%] hidden items-center justify-center mw-md:flex">
           <button
             className={`p-2 focus:outline-none flex items-center ml-4 border border-transparent rounded-full hover:bg-sky-300 hover:cursor-pointer transition-all duration-400`}
@@ -141,27 +121,16 @@ function MainHeader() {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
             </svg>
           </button>
         </div>
         <div
           className={`flex miw-md:w-[40vh] mw-md:h-[50vh] transition-all duration-300 justify-around items-center ml-5 mw-md:z-50 mw-md:${
             isMenuOpen
-<<<<<<< HEAD
               ? 'mw-md:flex-col mw-md:absolute mw-md:-left-10 mw-md:top-[70px] mw-md:bg-white mw-md:shadow-lg mw-md:rounded-lg mw-md:opacity-65 mw-md:hover:opacity-100'
               : 'hidden'
           }`}
-=======
-              ? "flex mw-md:flex-col mw-md:absolute mw-md:-left-10 mw-md:top-[4.4rem] mw-md:border-t-0 mw-md:rounded-r-lg mw-md:bg-slate-300"
-              : "hidden "
-          } mw-md:flex-col miw-md:flex ${isMenuOpen ? "" : " hidden"}`}
->>>>>>> 346d54e0735b8eefad92176e1fac3b06c2dedad6
           onClick={handleMenuClick}
         >
           {menus()}
@@ -219,10 +188,7 @@ function MainHeader() {
         </div>
       </div>
       {/* 헤더 검색 상자 */}
-      <div
-        id="search-box"
-        className={`w-1/2 h-full mw-md:3/4 mw-md:-ml-9 flex justify-center items-center`}
-      >
+      <div id="search-box" className={`w-1/2 h-full mw-md:3/4 mw-md:-ml-9 flex justify-center items-center`}>
         <div className="w-full flex justify-center">
           <input
             type="text"
@@ -267,19 +233,11 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <AuthProvider>
       <div className={`w-[100vw] h-[100vh] overflow-hidden`} id="main">
         {loading && <Loading />}
         <MainHeader />
         <div className="w-full h-full flex justify-center items-center mt-12 bg-white/55">
-=======
-    <div className="w-[100vw] h-[100vh] overflow-x-hidden" id="main">
-      {loading ? <Loading /> : ""}
-      <AuthProvider>
-        <MainHeader />
-        <div className="w-full h-full flex justify-center items-center mt-24">
->>>>>>> 346d54e0735b8eefad92176e1fac3b06c2dedad6
           <Routes>
             <Route exact path="/" element={<MainContent />}></Route>
             <Route exact path="/home" element={<MainContent />}></Route>
